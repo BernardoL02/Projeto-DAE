55c2cc708b485b6d514f3611131788514a7af9a1
Bernardo Lopes <bernaslopes0708.bl@gmail.com>
2024-10-19 11:14:37 +0100

    First

---
+++ amazonJBM/backend/src/main/java/com/example/amazonjbm/HelloResource.java
@@ -0,0 +1,14 @@
+package com.example.amazonjbm;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World! Bernardo123";
+    }
+}
