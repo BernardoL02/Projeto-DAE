c42d6b57b782fd2b8aad0f67f7c8144b1b348725
Miguel Crespo <miguelcrespovenancio@hotmail.com>
2024-10-22 14:08:20 +0100

    Projeto Base Fronted

---
+++ amazonJBM/frontend/ClientSAC/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+    <p class="text-4xl font-bold text-white animate-bounce hover:text-yellow-300 transition duration-500 ease-in-out transform hover:scale-110">
+      O jose borra se todo
+    </p>
+  </div>
+</template>
+
+<style>
+/* Adicione qualquer estilo global aqui, se necessário */
+</style>
